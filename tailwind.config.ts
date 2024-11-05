4e656a3a15b22f262968c68d772628430351124f
slince-zero <1711257704@qq.com>
2024-11-05 09:35:32 +0800

    chore: update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,32 +1,37 @@
-import type { Config } from "tailwindcss"
-import { fontFamily } from "tailwindcss/defaultTheme"
+import type { Config } from 'tailwindcss'
+import { fontFamily } from 'tailwindcss/defaultTheme'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       fontFamily: {
-        heading: ["var(--font-heading)", ...fontFamily.sans],
+        heading: ['var(--font-heading)', ...fontFamily.sans],
       },
       colors: {
         brand: {
-          25: "#F7FAFD",
-          50: "#F0F5FA",
-          100: "#E1EBF5",
-          200: "#C7D7EB",
-          300: "#A9C3DE",
-          400: "#8BAECF",
-          500: "#6D99BF",
-          600: "#5483A3",
-          700: "#426A86",
-          800: "#335269",
-          900: "#273E4F",
-          950: "#1C2D3A",
+          '25': '#F8F9FD',
+          '50': '#F0F4FA',
+          '100': '#E1E9F6',
+          '200': '#C3D3ED',
+          '300': '#A5BDE4',
+          '400': '#87A7DB',
+          '500': '#6991D2',
+          '600': '#4B76C9',
+          '700': '#3659B1',
+          '800': '#284189',
+          '900': '#1B2A61',
+          '950': '#111A3E',
         },
+        'discord-background': '#36393f',
+        'discord-brand-color': '#5865f2',
+        'discord-gray': '#36393f',
+        'discord-text': '#dcddde',
+        'discord-timestamp': '#72767d',
       },
     },
   },
