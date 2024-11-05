a2ea4db8d258f22dfa3e9a78f77b30bff4782b52
slince-zero <1711257704@qq.com>
2024-11-05 10:47:49 +0800

    feat: shinyButton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import { Heading } from './components/heading'
 import { MaxWidthWrappar } from './components/max-width-wrapper'
+import { Check } from 'lucide-react'
+import { ShinyButton } from './components/shiny-button'
 
 const Page = () => {
   return (
@@ -15,6 +17,44 @@ const Page = () => {
                   Delivered to Your Discord
                 </span>
               </Heading>
+            </div>
+
+            <>
+              <p className="text-base/7 text-gray-600 max-w-prose text-center text-pretty">
+                PingPanda is the easiest way to monitor your SaaS. Get instant
+                notifications for{' '}
+                <span className="font-semibold text-gray-700">
+                  sales, new users, or any other event
+                </span>{' '}
+                sent directly to your Discord.
+              </p>
+            </>
+
+            <>
+              <ul className="space-y-2 text-base/7 text-gray-600 text-left flex flex-col items-start">
+                {[
+                  'Real-time Discord alerts for critical events',
+                  'Buy once, use forever',
+                  'Track sales, new users, or any other event',
+                ].map((item, index) => (
+                  <li
+                    key={index}
+                    className="flex gap-1.5 items-center text-left"
+                  >
+                    <Check className="size-5 shrink-0 text-brand-700" />
+                    {item}
+                  </li>
+                ))}
+              </ul>
+            </>
+
+            <div className="w-full max-w-80">
+              <ShinyButton
+                href="/sign-up"
+                className="relative z-10 h-14 w-full text-base shadow-lg transition-shadow duration-300 hover:shadow-xl"
+              >
+                Start For Free Today
+              </ShinyButton>
             </div>
           </div>
         </MaxWidthWrappar>
