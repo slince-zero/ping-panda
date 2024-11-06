23d38d60e1c6ec654018b45c7f6c52b4c7ff21d6
slince-zero <1711257704@qq.com>
2024-11-06 16:28:15 +0800

    styles: update

--- src/components/ui/animated-list.tsx
+++ src/components/ui/animated-list.tsx
@@ -30,7 +30,7 @@ export const AnimatedList = React.memo(
     }, [childrenArray, messages, delay])
 
     return (
-      <div className={`flex flex-col items-center gap-4 ${className}`}>
+      <div className={`flex flex-col-reverse items-center gap-4 ${className}`}>
         <AnimatePresence>
           {messages.map((item) => (
             <AnimatedListItem key={(item as ReactElement).key}>
