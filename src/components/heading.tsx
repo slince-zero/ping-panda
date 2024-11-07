121226f302af271572877dea0f03e79ceb2f74cb
slince-zero <1711257704@qq.com>
2024-11-07 18:36:44 +0800

    feat: update landing page

--- src/components/heading.tsx
+++ src/components/heading.tsx
@@ -9,7 +9,7 @@ export const Heading = ({ children, className, ...props }: HeadingPorps) => {
   return (
     <h1
       className={cn(
-        'text-4xl sm:text-5xl tracking-tight text-pretty font-heading font-semibold text-zinc-800',
+        'text-[2.15rem] sm:text-5xl tracking-tight text-pretty font-heading font-semibold text-zinc-800',
         className
       )}
       {...props}
